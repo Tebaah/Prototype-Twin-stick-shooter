42f176fe4dac33a8840197664a68122a5d0dbddd
Tebaah <e2nd@protonmail.com>
2026-09-06 21:48:13 -0300

    fix(player): corregir herencia de PlayerController a CharacterBody2D

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -1,7 +1,7 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class PlayerController : Node
+public partial class PlayerController : CharacterBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
